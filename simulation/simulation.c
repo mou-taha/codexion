d08d5e593ba071ef560ee35a059cf9b0dd5d0926
taha <t.mousnia@factory.gr>
2026-09-02 15:35:08 +0100

    fix: restore pthread_create() loop and improve thread management in simulation

--- simulation/simulation.c
+++ simulation/simulation.c
@@ -6,10 +6,12 @@
 /*   By: tmousnia <tmousnia@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/02 07:08:23 by tmousnia          #+#    #+#             */
-/*   Updated: 2026/09/02 15:18:20 by tmousnia         ###   ########.fr       */
+/*   Updated: 2026/09/02 15:34:58 by tmousnia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+//TODO: One later issue remains: coders can block forever inside pthread_mutex_lock() while waiting for dongles after the monitor sets the stop flag. That will need to be replaced with condition-variable scheduling, but first restore the missing pthread_create() loop.
 #include "../codexion.h"
 
 void create_dongles_routine(t_data *data, t_coder *coders);
@@ -29,6 +31,9 @@ int start_simulation(t_data *data, t_simulation *simulation, t_coder *coders)
         pthread_mutex_lock(&coders[i].key);
         coders[i].last_compile_time = simulation->start_time;
         pthread_mutex_unlock(&coders[i].key);
+        if (pthread_create(&coders[i].thread_id, NULL,
+            &coder_routine, &coders[i]) != 0)
+        return (0);
         i++;
     }
 
@@ -38,11 +43,12 @@ int start_simulation(t_data *data, t_simulation *simulation, t_coder *coders)
     i = 0;
     while (i < data->nb_coders)
     {
-        if (pthread_create(&coders[i].thread_id, NULL,
-                &coder_routine, &coders[i]) != 0)
+        if (pthread_join(coders[i].thread_id, NULL) != 0)
             return (0);
         i++;
     }
+    if (pthread_join(monitor, NULL) != 0)
+        return (0);
     return (1) ;
 }
 
