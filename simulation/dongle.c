dcda328ab801660e590ce03d4a17a46049215d8b
taha1337 <t.mousnia@gmail.com>
2026-09-10 19:21:45 +0100

    revert to commit 178e369a38e4b1eb3a26c1d55d74dab816057ac2

--- simulation/dongle.c
+++ simulation/dongle.c
@@ -6,58 +6,38 @@
 /*   By: tmousnia <tmousnia@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/05 09:10:11 by tmousnia          #+#    #+#             */
-/*   Updated: 2026/09/10 18:25:03 by tmousnia         ###   ########.fr       */
+/*   Updated: 2026/09/09 12:01:30 by tmousnia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../codexion.h"
 
-static int	is_dongle_ready(t_coder *coder, t_dongle *dongle, long long now)
+
+void	grab_dongle(t_coder *coder, t_dongle *dongle)
 {
-	return (dongle->in_use == 0 && dongle->queue.size > 0
-		&& dongle->queue.nodes[0].coder->id == coder->id
-		&& now >= dongle->next_availability);
-}
+	long long	wait_time;
 
-static int	check_cooldown(t_coder *coder, t_dongle *d1, t_dongle *d2,
-		long long now)
-{
-	long long	wait;
-
-	wait = 0;
-	if (d1->in_use == 0 && d1->queue.size > 0
-		&& d1->queue.nodes[0].coder->id == coder->id
-		&& now < d1->next_availability)
-		wait = d1->next_availability - now;
-	else if (d2->in_use == 0 && d2->queue.size > 0
-		&& d2->queue.nodes[0].coder->id == coder->id
-		&& now < d2->next_availability)
-		wait = d2->next_availability - now;
-	if (wait > 0)
+	pthread_mutex_lock(&dongle->key);
+	while (1)
 	{
-		pthread_mutex_unlock(&d2->key);
-		pthread_mutex_unlock(&d1->key);
-		ft_usleep(wait, coder->simulation);
-		return (1);
-	}
-	return (0);
-}
-
-static void	wait_dongle_signal(t_coder *coder, t_dongle *d1, t_dongle *d2,
-		long long now)
-{
-
-	if (!is_dongle_ready(coder, d1, now))
-	{
-		pthread_mutex_unlock(&d2->key);
-		pthread_cond_wait(&d1->signal, &d1->key);
-		pthread_mutex_unlock(&d1->key);
-	}
-	else
-	{
-		pthread_mutex_unlock(&d1->key);
-		pthread_cond_wait(&d2->signal, &d2->key);
-		pthread_mutex_unlock(&d2->key);
+		if (dongle->in_use == 0 && dongle->queue.size != 0
+			&& dongle->queue.nodes[0].coder->id == coder->id)
+		{
+			if (get_current_time_ms() < dongle->next_availability)
+			{
+				wait_time = dongle->next_availability - get_current_time_ms();
+				pthread_mutex_unlock(&dongle->key);
+				ft_usleep(wait_time, coder->simulation);
+				pthread_mutex_lock(&dongle->key);
+				continue ;
+			}
+			// printf("\ncoder %d grab dongle %d\n",coder->id,dongle->id);
+			dongle->in_use = 1;
+			pop_coder(&dongle->queue);
+			pthread_mutex_unlock(&dongle->key);
+			return ;
+		}
+		pthread_cond_wait(&dongle->signal, &dongle->key);
 	}
 }
 
@@ -67,36 +47,22 @@ void	request_and_grab_dongles(t_coder *coder)
 	t_dongle	*d2;
 	long long	now;
 
-	d1 = coder->left_dongle;
-	d2 = coder->right_dongle;
-	if (coder->left_dongle->id > coder->right_dongle->id)
+	if (coder->id % 2 == 0)
 	{
 		d1 = coder->right_dongle;
 		d2 = coder->left_dongle;
 	}
-	create_dongle_request(coder, d1);
-	create_dongle_request(coder, d2);
-	while (!check_stop(coder->simulation))
+	else
 	{
-		pthread_mutex_lock(&d1->key);
-		pthread_mutex_lock(&d2->key);
-		now = get_current_time_ms();
-		if (is_dongle_ready(coder, d1, now) && is_dongle_ready(coder, d2, now))
-		{
-			d1->in_use = 1;
-			d2->in_use = 1;
-			pop_coder(&d1->queue);
-			pop_coder(&d2->queue);
-			pthread_mutex_unlock(&d2->key);
-			pthread_mutex_unlock(&d1->key);
-			print_status("has taken a dongle", coder);
-			print_status("has taken a dongle", coder);
-			return ;
-		}
-		if (check_cooldown(coder, d1, d2, now))
-			continue ;
-		wait_dongle_signal(coder, d1, d2, now);
+		first_dongle = coder->left_dongle;
+		second_dongle = coder->right_dongle;
 	}
+	create_dongle_request(coder, first_dongle);
+	grab_dongle(coder, first_dongle);
+	create_dongle_request(coder, second_dongle);
+	grab_dongle(coder, second_dongle);
+	print_status("has taken a dongle", coder);
+	print_status("has taken a dongle", coder);
 }
 
 void	drop_dongle(t_dongle *dongle, long long dongle_cooldown)
